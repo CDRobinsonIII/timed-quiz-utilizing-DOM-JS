127def920d44d1f6c09c68c25b724660571f5faf
Charles Robinson <the.real.robinsons@gmail.com>
2020-10-26 22:40:41 -0700

    For my own learning experience, I fixed the clear button and local storage issue. I also added some comments to the index.html page and the JavaScript page.

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,5 +1,17 @@
 /*jshint esversion: 6 */ 
 
+// Use DOM to grab the area on the High Scores page to insert the high scores and initials.
+var highScoreList = document.querySelector("#high-score-list");
+
+// Use DOM to grab clear storage button
+var clearStorageBtn = document.querySelector("#clear-scores");
+
+// Use DOM to grab navViewer area to turn it off and on.
+var navViewerEl = document.querySelector("#navViewer");
+
+// Use DOM to grab navViewer area to turn it off and on.
+var highScoreViewerEl = document.querySelector("#highScoreViewer");
+
 // Use DOM to grab button to start the quiz to use with addEventListner
 var startQuizBtn = document.querySelector("#quiz-start");
 
@@ -27,13 +39,17 @@ var questionBank = document.querySelector("#quiz-holder");
 // Use DOM to grab area to put final score.
 var finalScore = document.querySelector("#final-score");
 
+// Use DOM to grab the button that the user clicks on the index page to view high scores.
+var viewHighScoresBtn = document.querySelector("#view-highScores");
+
 //Use DOM to grab initials entered at the completion of the quiz.
-//Wasn't able to add High Score HTML.
-// var initialsEntered = document.querySelector("#initials");
+var initialsEntered = document.querySelector("#initials");
 
-// var initialsHighScore = [];
-// var initialsScore = [];
+// Create empty array to hold high score initials. Eventually will do this via a array of objects.
+var highScoresListInitials = [];
 
+// Create empty array to hold high scores. Eventually will do this via a array of objects.
+var highScoreListScores = [];
 
 // Creat varaiable question pool for 5 questions, using an array of objects.
 var questionPool = [{
@@ -53,7 +69,7 @@ var questionPool = [{
     possibleAnswers: ["Ridiculous","Unnecessary","A Nested Function","Unheard Of"],
     answerKey: "A Nested Function"
 },{
-    questionTitle:"What is better than coding in JavaScript",
+    questionTitle:"What is better than coding in JavaScript?",
     possibleAnswers: ["Everything","jQuery","Sleeping","Eating"],
     answerKey: "jQuery"
 }];
@@ -79,6 +95,7 @@ function startQuiz () {
         //Show the updated time on timer section on the main page. 
         timerDisplay.innerHTML = time;
 
+        // If timer runs out run the endQuiz function.
         if (time<=0) {
             endQuiz();
         }
@@ -88,7 +105,7 @@ function startQuiz () {
     createQuestion();
 }
 
-
+// Function to generate the question and answer choices via a template literal.
 function createQuestion() {
 
     //create question markUp in var
@@ -109,14 +126,17 @@ function createQuestion() {
     questionBank.innerHTML = questionMarkUp;
   }
 
-questionBank.addEventListener("click", function (event) {
+// This addEventListener waits for the user to click on their answer. Once they do, it calls the check answer function.
+if (questionBank) {
+    questionBank.addEventListener("click", function (event) {
     event.preventDefault();
     if (event.target.matches("button")) {
         checkAnswer(event);
     }
 });
+}
 
-
+// Functon to check the answer the user inputed against the answer key.
 function checkAnswer (event) {
 
     var theirAnswer = event.target.textContent;
@@ -152,13 +172,14 @@ function checkAnswer (event) {
     if (questionIndex === questionPool.length) {
             endQuiz ();
     }
-   
+           
+    // If there are still questions available display next question by calling the createQuestion function.
     else {
-        // Display next question by calling the createQuestion function.
         createQuestion ();
     }
 }
-    
+
+// This function will end the quiz and let the user know what score the received. 
 function endQuiz () {
     clearInterval(timer);
     quizQuestion.style.display="none";
@@ -166,17 +187,124 @@ function endQuiz () {
     finalScore.innerHTML = score;
 }
 
+// This function is called upon via the addEventListener for when the user hits the submit button it enter their initials.
 function addInitials (event) {
     event.preventDefault(event);
-    window.location = "high_scores.html";
 
-    // Wasn't able to create high scores page. 
-    // initialsEntered.push="initialsEntered.value";
-    // initialsScore.push="score";
+    // If user inputs nothing before they hit the submit button, return to have them enter it again.
+    if (initialsEntered.value === "") {
+        return;
+      }
+      
+    // Call the retrieve function to retrieve the high scores from the local storage.
+    retrieveHighScores();
+
+    // Once the stored scores are added to the high score and initials arrays, add the new score and inititals to the arrays.
+    var scoreToPush = (score).toString();
+    var initialsToPush = initialsEntered.value.toUpperCase();
+    highScoresListInitials.push(initialsToPush);
+    highScoreListScores.push(scoreToPush);
+
+    // Call the store high score function to store the updated high scores array to the local storage.
+    storeHighScores();
+
+    // Call the retrieve function to retrieve the newly updated high scores so you can render update high score to high score section.
+    retrieveHighScores();
+}
+
+// This function will retrieve any high scores and initials from local storage.
+function retrieveHighScores() {
+
+    // Get stored scores and initials from localStorage.
+    // Remember in order to store objects in local or session storage you first have to convert them to a strong with JSON.stringify. 
+    // Because they were converted to a string, when you retrieve them they have to be converted back to a object. 
+    // You do that with this line of code.
+    // Parsing the JSON string to an object
+    var storedScores = JSON.parse(localStorage.getItem("highScoresListInitials"));
+    var storedInitials = JSON.parse(localStorage.getItem("highScoreListScores"));
+
+    // If scores and initials were retrieved from localStorage, update the highScoresListInitials and highScoreListScores arrays to it
+    // !== means if not empty
+    if (storedScores !== null) {
+        highScoresListInitials = storedScores;
+        highScoreListScores = storedInitials;
+    }
+
+    // Render high scores to the high scores section, if there weren't any, none will be displayed.
+    renderHighScores();
+  }
+
+// Function to render high scores on high score list by using DOM to grab high score list ID and adding names and scores to it. 
+function renderHighScores () {
+
+    quizSection.style.display="none";
+    quizQuestion.style.display="none";
+    quizOver.style.display="none";
+    navViewerEl.style.display="none";
+    highScoreViewerEl.style.display="block";
+
+    // This will clear the high score so that we can add the updated high score list array.
+    highScoreList.innerHTML = "";
+
+    // Determine how many entries are in the high score array. Can use either the highScoresListName or highScoresListScore array as they are the same length. 
+    // The goal after learning this is to change this to a more efficient manner by using an array of objects that include the property name and property score. 
+    var highScoreEntries = highScoreListScores.length;
+
+    for (var i =0; i < highScoreEntries; i++) {
+
+        // Create variables to hold the current index name and score of the entry. 
+        var highScoreEntryInitials = highScoresListInitials[i];
+        var highScoreEntryScores = highScoreListScores[i];
+
+        // Combine highScoreEntryInitials and highScoreEntryScores into one variable highScoreInitialsAndScore.
+        var highScoreEntryInitialsAndScore = (highScoreEntryInitials + "    Score: " + highScoreEntryScores);
+
+        // Create variable li to hold the created li element which we will be adding to the high score list on the high score html page.
+        var li = document.createElement("li");
+
+        // Set li textContent to the highScoreEntryInitialsAndScore variable that includes the initials and score.
+        li.textContent = highScoreEntryInitialsAndScore;
+
+        // Now using the high score element variable that knows where to insert li on the high score page, append it to the ol.
+        highScoreList.appendChild(li);
+    }
+}
+
+// Function to store high scores and initials arrays to local storage. 
+function storeHighScores() {
+    // Stringify and set high scores and initials arrays in localStorage
+    localStorage.setItem("highScoresListInitials", JSON.stringify(highScoresListInitials));
+    localStorage.setItem("highScoreListScores", JSON.stringify(highScoreListScores));   
+}
+
+//Function to clear the local storage. Clear it first, them redeclare high scores and initials arrays to empty and render to high score section.
+function clearLocalStorage () {
+    var confirmClear = confirm("Are you sure you want to clear the high scores?");
+    if (confirmClear) {
+        localStorage.clear();
+        highScoresListInitials = [];
+        highScoreListScores = [];
+        renderHighScores();
+    }
+    else {
+        return;
+    }
 }
 
 // Add event listener for when the user clicks on the start quiz button.
-startQuizBtn.addEventListener("click", startQuiz);
+if (startQuizBtn) {
+    startQuizBtn.addEventListener("click", startQuiz);
+}
 
 // Add event listener for when the user clicks the submit button to add their initials.
-submitInitialsBtn.addEventListener("click", addInitials);
+if (submitInitialsBtn) {
+    submitInitialsBtn.addEventListener("click", addInitials);
+}
+// Add event listener for when the user clicks the high scores button on the home page to retrieve and render high scores.
+if (viewHighScoresBtn) {
+    viewHighScoresBtn.addEventListener("click", retrieveHighScores);
+}
+// Add event listener for when the user clicks on the clear the high scores button.
+if (clearStorageBtn) {
+    clearStorageBtn.addEventListener("click", clearLocalStorage);
+}
